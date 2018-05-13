a6a9881135f40438d6a2a15d8882e4e59ec832fd
Aviraj Khare <avirajkhare00@gmail.com>
2018-05-14 01:56:32 +0530

    basic code complete

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 import os
 
-#give a directory root
-# give a file extension we want to catch
-# store all those files inside an array
-
 def input_dir(dir_name):
 
     for root, dirs, files in os.walk(dir_name, topdown=False):
         for name in files:
-            #print(os.path.join(root, name))
             if os.path.splitext(os.path.join(root, name))[-1] == '.mp4':
-                print os.path.join(root, name)
-                #send all above filenames for ffmpeg conversion
-                filter_video_files(os.path.join(root, name))
-        #since we are not printing dirs
-        #for name in dirs:
-        #    print(os.path.join(root, name))
+                if name.split('_')[-1] == "compressed.mp4":
+                    pass
+                else:
+                    filter_video_files(os.path.join(root, name))
 
 def filter_video_files(video_file_path):
     
     print video_file_path
-    #we will call os.system() for ffmpeg compression
-    #and will save it in same folder
+    
+    command = "ffmpeg -i " +  video_file_path + " -f mp4 -vcodec libx264 -preset medium -profile:v main -acodec aac " + video_file_path.split('.')[-2] + "_compressed.mp4"  + " -hide_banner"
+    
+    os.system(command)
+
+    os.remove(video_file_path)
 
 #run the function
 
-input_dir('/Users/avirajkhare00/Music')
+input_dir('./')
